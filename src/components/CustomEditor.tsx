529e41aac0e44a1114df86f86ef359fc43ea297d
BuildTheTruth <dkwk1254@naver.com>
2024-10-03 23:47:14 +0900

    ToastUIEditor forwardRef 적용

--- src/components/CustomEditor.tsx
+++ src/components/CustomEditor.tsx
@@ -5,15 +5,20 @@ import 'tui-color-picker/dist/tui-color-picker.css'
 
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
 import { Editor, EditorProps } from '@toast-ui/react-editor'
-import { useRef } from 'react'
+import { MutableRefObject, useRef } from 'react'
 
-export default function CustomEditor({ onChange, ...props }: EditorProps) {
+interface Props extends EditorProps {
+  forwardedRef?: MutableRefObject<Editor>
+}
+
+export default function CustomEditor({ onChange, forwardedRef, ...props }: Props) {
   const editorRef = useRef<Editor>(null)
+  const ref = forwardedRef ?? editorRef
 
   const handleChange = () => {
-    const html = editorRef.current?.getInstance().getHTML()
+    const html = ref.current?.getInstance().getHTML()
     onChange?.(html)
   }
 
-  return <Editor plugins={[colorSyntax]} onChange={handleChange} {...props} ref={editorRef} />
+  return <Editor plugins={[colorSyntax]} onChange={handleChange} {...props} ref={ref} />
 }
--- src/components/ToastUIEditor.tsx
+++ src/components/ToastUIEditor.tsx
@@ -1,5 +1,7 @@
 import { uploadFileToStorage } from '@/libs/firebase/storage'
+import { Editor } from '@toast-ui/react-editor'
 import dynamic from 'next/dynamic'
+import { forwardRef, MutableRefObject } from 'react'
 
 const DynamicEditor = dynamic(() => import('@/components/CustomEditor'), { ssr: false })
 
@@ -10,26 +12,31 @@ interface Props {
   onChange?: (value: string) => void
 }
 
-export default function ToastUIEditor({ value, authorId, storageImagePath, onChange }: Props) {
-  if (typeof window === 'undefined') {
-    return null
-  }
+const ToastUIEditor = forwardRef<Editor, Props>(
+  ({ value, authorId, storageImagePath, onChange }, ref) => {
+    if (typeof window === 'undefined') {
+      return null
+    }
 
-  const addImageBlobHook = async (file: File, callback: (url: string, text: string) => void) => {
-    const url = await uploadFileToStorage({ id: authorId, path: storageImagePath, file })
-    callback(url, file.name)
-  }
+    const addImageBlobHook = async (file: File, callback: (url: string, text: string) => void) => {
+      const url = await uploadFileToStorage({ id: authorId, path: storageImagePath, file })
+      callback(url, file.name)
+    }
 
-  return (
-    <DynamicEditor
-      initialValue={value}
-      previewStyle="vertical"
-      height="100%"
-      initialEditType="wysiwyg"
-      useCommandShortcut={false}
-      onChange={onChange}
-      language="ko-KR"
-      hooks={{ addImageBlobHook }}
-    />
-  )
-}
+    return (
+      <DynamicEditor
+        initialValue={value}
+        previewStyle="vertical"
+        height="100%"
+        initialEditType="wysiwyg"
+        useCommandShortcut={false}
+        onChange={onChange}
+        language="ko-KR"
+        hooks={{ addImageBlobHook }}
+        forwardedRef={ref as MutableRefObject<Editor>}
+      />
+    )
+  }
+)
+
+export default ToastUIEditor
